a9bcea896403bf6347fa4a1d480360c02b6f5a7f
dsyouness <y.drissislimani@gmail.com>
2020-02-06 11:12:57 +0100

    add views

--- App/models.py
+++ App/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    nom = models.CharField(max_length=10)
+    prix = models.DecimalField(max_digits=4, decimal_places=4)
--- App/views.py
+++ App/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+def index_view(request):
+    context = {'liste': [1, 2, 3],
+
+               }
+    return render(request, 'index.html', context)
